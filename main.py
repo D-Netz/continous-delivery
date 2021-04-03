ce2b86ab53b61f2ba519722b9aba70d8391c3f7c
wintermu_ <wintermu_@outlook.com>
2021-04-03 06:13:54 +0000

    added new route

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #push
 from flask import Flask
 from flask import jsonify
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -24,6 +25,11 @@ def bob():
 def babs():
     val = {"value": "babs"}
     return jsonify(val)
+
+@app.route('/pandas')
+def pandas_sugar():
+    df = pd.read_csv("https://raw.githubusercontent.com/noahgift/sugar/master/data/education_sugar_cdc_2003.csv")
+    return jsonify(df.to_dict())
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
